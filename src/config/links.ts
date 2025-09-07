1a762909279343d4f32df59e31744beea7cd31b7
fagaalves <faga.alves@gmail.com>
2025-09-06 23:45:32 -0300

    build errors debug

--- src/config/links.ts
+++ src/config/links.ts
@@ -8,6 +8,7 @@ export const LINKS = {
   // External links
   GITHUB_ORG: 'https://github.com/PlayfulProcess',
   GITHUB_REPO: 'https://github.com/PlayfulProcess/recursive-channels-fresh',
+  GITHUB_WELLNESS: 'https://github.com/PlayfulProcess/jongu-wellness',
   
   // Social links
   DISCORD: 'https://discord.com/invite/qn69cSbDez',
